cd2e7f5502287bd392e1b928d904b4fa0f364dbe
luminosoda <luminosoda@youngwaves.ru>
2021-11-06 17:24:46 +0500

    Implemented new methods

--- src/bot/api.rs
+++ src/bot/api.rs
@@ -469,6 +469,30 @@ impl Requester for Bot {
         )
     }
 
+    type ApproveChatJoinRequest = JsonRequest<payloads::ApproveChatJoinRequest>;
+
+    fn approve_chat_join_request<C>(&self, chat_id: C, user_id: i64) -> Self::ApproveChatJoinRequest
+    where
+        C: Into<ChatId>,
+    {
+        Self::ApproveChatJoinRequest::new(
+            self.clone(),
+            payloads::ApproveChatJoinRequest::new(chat_id, user_id),
+        )
+    }
+
+    type DeclineChatJoinRequest = JsonRequest<payloads::DeclineChatJoinRequest>;
+
+    fn decline_chat_join_request<C>(&self, chat_id: C, user_id: i64) -> Self::DeclineChatJoinRequest
+    where
+        C: Into<ChatId>,
+    {
+        Self::DeclineChatJoinRequest::new(
+            self.clone(),
+            payloads::DeclineChatJoinRequest::new(chat_id, user_id),
+        )
+    }
+
     type SetChatPhoto = MultipartRequest<payloads::SetChatPhoto>;
 
     fn set_chat_photo<C>(&self, chat_id: C, photo: InputFile) -> Self::SetChatPhoto
